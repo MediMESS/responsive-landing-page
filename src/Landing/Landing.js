2a2f7f5a0905a489a3798b8ed25578d5953d0013
MediMESS <medimessarat@gmail.com>
2021-09-22 22:17:56 +0100

    Structuring project into Features/components

---
+++ src/Landing/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "./components/Hero/Hero";
+
+const Landing = () => {
+  return (
+    <React.Fragment>
+      <Hero />
+      <h1>Landing Page</h1>
+    </React.Fragment>
+  );
+};
+
+export default Landing;
---
+++ src/Landing/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div>Picture</div>
+      <div className="section-container">Hero Section</div>
+    </div>
+  );
+};
+
+export default Hero;
