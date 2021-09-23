09c93a757642c069410618ac77aaa16b9b003a8b
MediMESS <medimessarat@gmail.com>
2021-09-23 07:29:04 +0100

    Adding Hero section

--- src/Landing/components/Hero/Hero.js
+++ src/Landing/components/Hero/Hero.js
@@ -1,9 +1,13 @@
 import React from "react";
+import heroClasses from "./Hero.module.css";
+import heroBackgroundPath from "./../../../assets/hero/curved_background.svg";
 
 const Hero = () => {
   return (
-    <div>
-      <div>Picture</div>
+    <div className={heroClasses.hero}>
+      <div className={heroClasses["hero-background"]}>
+        <img src={heroBackgroundPath} alt="hero-background" />
+      </div>
       <div className="section-container">Hero Section</div>
     </div>
   );
