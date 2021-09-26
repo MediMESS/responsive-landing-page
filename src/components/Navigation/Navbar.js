0adc3a90d8ef58d6aface0b77d9a3791a66d00db
MediMESS <medimessarat@gmail.com>
2021-09-26 22:19:58 +0100

    setting the logo in the navbar

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -6,11 +6,11 @@ const Navbar = () => {
   return (
     <div>
       <div className={navbarStyles.logo}>
-        <span>C</span>
-        <span>o</span>
-        <span>L</span>
-        <span>L</span>
-        <span>.</span>
+        <span className="primary">C</span>
+        <span className="primary">o</span>
+        <span className="dark-primary">L</span>
+        <span className="dark-primary">L</span>
+        <span className="yellow">.</span>
       </div>
       <h1>Navbar</h1>
     </div>
