ba45872dac9a1787abe93d989f74646f7d3750f6
MediMESS <medimessarat@gmail.com>
2021-09-26 22:16:53 +0100

    setting global colors selectors

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,8 +1,17 @@
 import React from "react";
+import * as navbarStyles from "./Navbar.module.css";
+import logoPath from "./../../assets/hero";
 
 const Navbar = () => {
   return (
     <div>
+      <div className={navbarStyles.logo}>
+        <span>C</span>
+        <span>o</span>
+        <span>L</span>
+        <span>L</span>
+        <span>.</span>
+      </div>
       <h1>Navbar</h1>
     </div>
   );
